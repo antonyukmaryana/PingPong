58651fa8fcca4ba8687e8567980b6785b94ac20c
Maryana Antonyuk and Sarah Angel <sarah@>
2019-04-23 13:36:25 -0700

    correct syntax of test method

--- PingPongTests/ModelTests/PingPongTest.cs
+++ PingPongTests/ModelTests/PingPongTest.cs
@@ -7,20 +7,20 @@ namespace PingGame.Tests
   public class PingPongTest
   {
     [TestMethod]
-    public void IsPingPong_NumberDivisibleByThree_True()
+    public void IsPingPong_NumberDivisibleByThree_Ping()
     {
       PingPong testPingPong = new PingPong();
       Assert.AreEqual("ping", testPingPong.IsPingPong(3));
     }
     [TestMethod]
-    public void IsPingPong_NumberDivisibleByFive_True()
+    public void IsPingPong_NumberDivisibleByFive_Pong()
     {
       PingPong testPingPong = new PingPong();
       Assert.AreEqual("pong", testPingPong.IsPingPong(5));
 
     }
     [TestMethod]
-    public void IsPingPong_NumberDivisibleByThreeAndFive_True()
+    public void IsPingPong_NumberDivisibleByThreeAndFive_PingPong()
     {
       PingPong testPingPong = new PingPong();
       Assert.AreEqual("pingpong", testPingPong.IsPingPong(15));
